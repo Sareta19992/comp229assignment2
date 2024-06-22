008fcb039753ff1e1d705149d2c01aba3ea5ee18
Sara1992115 <shamolei@my.centennialcollege.ca>
2024-06-21 22:19:21 -0400

    first commit

---
+++ server/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
